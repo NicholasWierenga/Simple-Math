757910b21c0221e3df85282e218bb5d075dbaef7
Nicholas Wierenga <102650184+NicholasWierenga@users.noreply.github.com>
2022-03-30 05:27:15 -0400

    Add files via upload

---
+++ Prime Numbers.sql
@@ -0,0 +1,83 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    primenum     NUMBER(6) := 0;
+    primecount   NUMBER(6) := 0;
+    primelist    VARCHAR2(10000) := 'The primes in this range are: ';
+BEGIN
+    FOR x IN 1..10000 LOOP FOR y IN 2..x LOOP
+        IF y = x THEN
+            primenum := x;
+            primecount := primecount + 1;
+            primelist := primelist
+                         || primenum
+                         || ', '; --adds prime to list
+        END IF;
+
+        IF x MOD y = 0 THEN
+            EXIT; --to exit the inner loop after a factor of x is found
+        END IF;
+    END LOOP;
+    end LOOP;
+
+    dbms_output.put_line(substr(primelist, 1, length(primelist) - 2)
+                         || '.');
+
+END; -- This results in a string with all the prime numbers in the range in them.
+
+CREATE GLOBAL TEMPORARY TABLE primelist (
+    prime NUMBER(6)
+);
+
+--This is the above block, but inserts the list into Primelist instead of a string.
+
+DECLARE
+    primenum NUMBER(6) := 0;
+BEGIN
+    FOR x IN 1..10000 LOOP
+        FOR y IN 2..x LOOP
+            IF y = x THEN
+                primenum := x;
+                INSERT INTO primelist VALUES ( primenum );
+
+            END IF;
+
+            IF x MOD y = 0 THEN
+                EXIT; --to exit the inner loop after a factor of x is found
+            END IF;
+        END LOOP;
+    END LOOP;
+END;
+
+-- Below can test for primes using values from the table.
+
+DECLARE
+    testprime   NUMBER(6) := 81;
+    primes      NUMBER(6);
+BEGIN
+    INSERT INTO primelist VALUES ( 0 );
+
+    SELECT
+        MAX(prime)
+    INTO primes
+    FROM
+        primelist
+    WHERE
+        prime IN (
+            0,
+            testprime
+        );
+
+    DELETE FROM primelist
+    WHERE
+        prime = 0; -- Inserting 0 was only to avoid the null row error.
+
+    IF primes = 0 THEN
+        dbms_output.put_line(testprime || ' is not prime.');
+    ELSE
+        dbms_output.put_line(testprime || ' is prime.');
+    END IF;
+
+END;
+
+DROP TABLE primelist;
